015b4623aeb72e663b534fa51f06eceaf8204cc5
Joshua Lee <joshualee@joshuas-air-2.home>
2020-09-21 17:28:52 -1000

    Updates

---
+++ precache-manifest.ea20f7b1e3edd22cd3a5818217e054db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d92761ab9a3ab5997b751ea01495f70",
+    "url": "/react-hooks-todolist/index.html"
+  },
+  {
+    "revision": "c93ba5d3586ef560206e",
+    "url": "/react-hooks-todolist/static/css/main.7ebc0670.chunk.css"
+  },
+  {
+    "revision": "25676beef8bcbc7d4581",
+    "url": "/react-hooks-todolist/static/js/2.3f90febc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-hooks-todolist/static/js/2.3f90febc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93ba5d3586ef560206e",
+    "url": "/react-hooks-todolist/static/js/main.d9b75c65.chunk.js"
+  },
+  {
+    "revision": "2c448e407a377860c6a8",
+    "url": "/react-hooks-todolist/static/js/runtime-main.6d70ab1e.js"
+  },
+  {
+    "revision": "821d2ea6f14db2b1c4b3a8f9178b0108",
+    "url": "/react-hooks-todolist/static/media/grid.821d2ea6.png"
+  }
+]);
